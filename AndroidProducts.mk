5c06e27e41a04ddc501ec6ed4b2c10620126b635
Hendy Augustama <45126844+henmadx@users.noreply.github.com>
2020-04-13 23:13:35 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_capricorn.mk
+    $(LOCAL_DIR)/aosp_capricorn.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_capricorn-user \
-    havoc_capricorn-userdebug \
-    havoc_capricorn-eng
+    aosp_capricorn-user \
+    aosp_capricorn-userdebug \
+    aosp_capricorn-eng
