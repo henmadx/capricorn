4fd6d203b647fd06b43e03f41ba8da50be553fda
Hendy Augustama <45126844+henmadx@users.noreply.github.com>
2020-05-07 01:58:53 +0700

    capricorn: Prebuild GCam [1]

--- device.mk
+++ device.mk
@@ -29,9 +29,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml
 
-# Camera
+# Camera app
 PRODUCT_PACKAGES += \
-    Snap
+    MGC
 
 # ConfigPanel
 PRODUCT_PACKAGES += \
